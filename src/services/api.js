ee6c674e38359efc6d789fa0ce1c85ae39361b7c
gstadtler <ghstadtler@gmail.com>
2021-08-30 19:58:33 -0300

    Uses env variables

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "202174702aa670c3e7ef10d429ba58b8";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default axios.create({
   baseURL: "https://api.openweathermap.org/data/2.5",
